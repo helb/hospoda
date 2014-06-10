d3fdbfc2c151f6d90658f8281c6ed9afe0677e7f
helb <helb@helb.cz>
2014-06-10 10:14:47 +0200

    Moved routes and models to lib/

--- routes.js
+++
@@ -1,5 +0,0 @@
-Router.map(function() {
-  this.route('hospoda', {path: '/hospoda', onAfterAction: function(){document.title = 'hospoda'}})
-  this.route('kuchyne', {path: '/kuchyne', onAfterAction: function(){document.title = 'kuchyně'}})
-  this.route('report', {path: '/report', onAfterAction: function(){document.title = 'tabulka'}})
-});
